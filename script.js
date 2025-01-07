d7b7523a6c4ff07af61a666d77909e7fc099082f
James Hawker <526rserbinenko@frhsd.com>
2025-01-07 19:59:32 +0000

    animappend doesn't work for some reason

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = digitize();
-
 function digitize() {
   let across = 10;
   let vert = 7;
@@ -7,7 +5,12 @@ function digitize() {
   base.setAttribute("style", "background: green;");
   let container = document.getElementById("digiContainer");
   let divMeasure = Math.floor(base.offsetWidth / across);
-  base.setAttribute("style", `height: ${divMeasure * vert}px;`);
+  base.setAttribute(
+    "style",
+    `height: ${divMeasure * vert}px !important; width: ${
+      base.offsetWidth
+    }px !important; background: transparent; border: none;`
+  );
   let divArr = [];
   for (let i = 0; i < vert; i++) {
     divArr.push(new Array());
@@ -25,4 +28,52 @@ function digitize() {
     }
     container.append(row);
   }
+  let style = document.getElementsByTagName("style")[0];
+  for (let i = 0; i < divArr.length; i++) {
+    for (let q = 0; q < divArr[i].length; q++) {
+      let anim = `@keyframes box_${i}_${q} {
+        0% {
+      translate: scale(0.8);
+      left: -30px;
+      right: 0;
+        }
+        20% {
+      translate: scale(0.4);
+      left: 0;
+      right: -30px;
+        }
+        40% {
+      translate: scale(1.2);
+      left: -30px;
+      right: 0;
+        }
+        60% {
+      translate: scale(0.7);
+      left: 0;
+      right: -30px;
+        }
+        80% {
+      translate: scale(0.1);
+      left: -30px;
+      right: 0;
+        }
+        100% {
+      translate: scale(0.5);
+      left: 0;
+      right: -30px;
+        }
+      }`;
+      let animClass = `#box_${i}_${q} {
+      background: red !important;
+      animation-name: box_${i}_${q};
+      animation-duration: 2s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      animation-delay: ${i + q}s;
+      }`;
+      style.insertAdjacentHTML("beforebegin", animClass);
+      style.insertAdjacentHTML("beforebegin", anim);
+    }
+  }
+  alert(style.innerHTML);
 }
