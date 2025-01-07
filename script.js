19c6331c2624bff401888fbb0445ff0bde62a2ca
James Hawker <526rserbinenko@frhsd.com>
2025-01-07 18:48:37 +0000

    working on digitize

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = digitize();
+
+function digitize() {
+  let across = 10;
+  let vert = 4;
+  let base = document.getElementById("digitize");
+  base.setAttribute("style", "background: green;");
+  let divMeasure = Math.floor(base.offsetWidth / across);
+  base.setAttribute("style", `height: ${divMeasure * vert}px;`);
+  let divArr = [];
+  for (let i = 0; i < vert; i++) {
+    divArr.push(new Array());
+    let row = document.createElement("div");
+    row.classList.add("digiRow");
+    for (let q = 0; q < across; q++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
+      box.id = `box_${i}_${q}`;
+      box.setAttribute(
+        "style",
+        `height: ${divMeasure}px; width: ${divMeasure}`
+      );
+      row.append(box);
+    }
+    base.append(row);
+  }
+}
